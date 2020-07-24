ee12d5158831b3e912ba89d5fa6978834c011fec
Catalina Peralta <caperal@microsoft.com>
2020-07-24 15:04:29 -0700

    wip

--- sdk/internal/duration/durations_test.go
+++ sdk/internal/duration/durations_test.go
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package durations
+package duration
 
 import (
 	"testing"
@@ -9,35 +9,35 @@ import (
 )
 
 func TestParseDuration(t *testing.T) {
-	p, err := ParseDuration("P123DT22H14M12.011S")
+	p, err := Parse("P123DT22H14M12.011S")
 	if err != nil {
 		t.Fatal(err)
 	}
 	if p != time.Duration(10707252011000000) {
 		t.Fatalf("Did not receive the correct value for the duration.\nExpected: 10707252011000000, Received: %s", p.String())
 	}
-	p, err = ParseDuration("PT12H")
+	p, err = Parse("PT12H")
 	if err != nil {
 		t.Fatal(err)
 	}
 	if p != time.Duration(12*time.Hour) {
 		t.Fatalf("Did not receive the correct value for the duration.\nExpected: 10707252011000000, Received: %s", p.String())
 	}
-	p, err = ParseDuration("PT0S")
+	p, err = Parse("PT0S")
 	if err != nil {
 		t.Fatal(err)
 	}
 	if p != time.Duration(0) {
 		t.Fatalf("Did not receive the correct value for the duration.\nExpected: 10707252011000000, Received: %s", p.String())
 	}
-	p, err = ParseDuration("PT12.5H10S")
+	p, err = Parse("PT12.5H10S")
 	if err != nil {
 		t.Fatal(err)
 	}
 	if p != time.Duration(12.5*float64(time.Hour)+float64(10*time.Second)) {
 		t.Fatalf("Did not receive the correct value for the duration.\nExpected: 10707252011000000, Received: %s", p.String())
 	}
-	p, err = ParseDuration("PT0.5S")
+	p, err = Parse("PT0.5S")
 	if err != nil {
 		t.Fatal(err)
 	}
